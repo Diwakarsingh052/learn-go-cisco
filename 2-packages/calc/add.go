163124c23a96c9662384608d905745871ef020d7
Diwakar Singh <diwakarsingh052@gmail.com>
2025-11-12 10:33:03 +0530

    .

--- 2-packages/calc/add.go
+++ 2-packages/calc/add.go
@@ -7,4 +7,13 @@ import "fmt"
 // making first letter capital makes the function public
 func Add(a int, b int) {
 	fmt.Println(a + b)
+
 }
+
+// create a new file named as print.go
+// create a function named print (unexported)
+// instead of using fmt.Println in Add function, use your custom print function
+
+// create a new package named as format
+// create a exported function that appends ** to the string
+//str = "s" + "**"
