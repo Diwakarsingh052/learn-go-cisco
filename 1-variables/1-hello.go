f62856741ecc038b0b303716a18d613ecd2160d3
Diwakar Singh <diwakarsingh052@gmail.com>
2025-11-12 09:57:59 +0530

    .

---
+++ 1-variables/1-hello.go
@@ -0,0 +1,13 @@
+// main is an executable package
+package main
+
+// library package
+// lib package can't be run,
+
+// "fmt" is a package from standard library
+import "fmt"
+
+// main function is the entry point of the program
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module learn-go-cisco
+
+go 1.25.1
