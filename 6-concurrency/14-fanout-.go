ac55f7222a36fb275810441b941254582dbd9daf
Diwakar Singh <diwakarsingh052@gmail.com>
2025-11-13 15:07:31 +0530

    .

--- 6-concurrency/14-fanout-.go
+++ 6-concurrency/14-fanout-.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // close the channel at the correct place
 // and use waitgroups to wait for all goroutines to finish
@@ -8,18 +11,27 @@ import "fmt"
 func main() {
 	ch := make(chan int)
 
-	go func() {
-		for i := 1; i <= 4; i++ {
-			go func() {
-				ch <- i
-			}()
-		}
-	}()
+	wg := new(sync.WaitGroup)
+	wgTask := new(sync.WaitGroup)
 
-	go func() {
+	wg.Go(func() {
+		for i := 1; i <= 4; i++ {
+			// fan out pattern
+			// spinning n number of goroutines for n tasks
+			wgTask.Go(func() {
+				ch <- i
+			})
+		}
+		// wait for all goroutines to finish
+		wgTask.Wait()
+		close(ch) // close the channel once all tasks are done
+
+	})
+
+	wg.Go(func() {
 		for v := range ch {
 			fmt.Println(v)
 		}
-	}()
-
+	})
+	wg.Wait()
 }
