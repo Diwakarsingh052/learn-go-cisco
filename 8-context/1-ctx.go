60a30591163342cd77b4f41248b11e62950f61bc
Diwakar Singh <diwakarsingh052@gmail.com>
2025-11-13 16:19:10 +0530

    .

---
+++ 8-context/1-ctx.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+// context allow us to cancel the work if it does not finish in provided time
+func main() {
+	// Background context is empty context which is used to store timeout values
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Millisecond)
+	defer cancel()
+	// constructing a new request with context
+	// we have not made the request yet to the remote server
+	req, err := http.NewRequestWithContext(
+		ctx, http.MethodGet, "https://www.google.com", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// doing the request to the remote server
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// reading the response body in bytes
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// printing the response body
+	log.Println(string(bytes))
+
+}
