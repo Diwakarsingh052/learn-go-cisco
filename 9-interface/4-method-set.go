e1ce65f00c0790980e6686a637b8cefd2b329c98
Diwakar Singh <diwakarsingh052@gmail.com>
2025-11-14 14:17:13 +0530

    .

---
+++ 9-interface/4-method-set.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Speaker interface {
+	Speak() string
+	Speak2() string
+}
+
+type Person struct {
+	name string
+}
+
+func (p *Person) Speak() string {
+	return fmt.Sprintf("Hi, my name is %s", p.name)
+}
+func (p Person) Speak2() string {
+	return fmt.Sprintf("Hi, my name is 2 %s", p.name)
+}
+
+func main() {
+	var p Person       //[Speak2]
+	var s Speaker = &p // [Speak, Speak2]
+	s.Speak()
+
+}
+
+/*
+                          +-------------------------------------+
+                          |              Method Set             |
+                          +------------------+------------------+
+                          |    Value Type    |   Pointer Type   |
++-------------------------+------------------+------------------+
+| Function w/ Value Rec.  |        Yes       |       Yes        |
+| ( func (t T) )          |                  |                  |
++-------------------------+------------------+------------------+
+| Function w/ Ptr Rec.    |        No        |       Yes        |
+| ( func (t *T) )         |                  |                  |
++-------------------------+------------------+------------------+
+
+If a function is implemented with a value receiver (func (t T)),
+it can be called through a value or a pointer.
+
+If a function is implemented with a pointer receiver (func (t *T)),
+it can only be called through a pointer.
+*/
